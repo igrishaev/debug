ec0e5b021fa8c63efbde809deef371341ef3a083
Ivan Grishaev <ivan@grishaev.me>
2017-03-17 10:16:36 +0300

    some experiments

--- src/debug/core.clj
+++ src/debug/core.clj
@@ -1,6 +1,18 @@
 (ns debug.core
   (:gen-class))
 
+(defmacro repl* []
+  `((fn [] ~(read))))
+
+;; (defn repl2 []
+;;   (let [body (read)]
+;;     (fn [] )
+;;     )
+;;   `((fn [] ~(read))))
+
+;; (defn repl3 []
+;;   (repl*))
+
 (defn repl []
   (let [input (read)]
     (if (= input 'q)
@@ -10,42 +22,52 @@
           (println result)
           (recur))))))
 
+(def repl-code
+  `(loop []
+     (let [input# (read)]
+       (if (= input# :q)
+         nil
+         (let [result# (deref (delay input#))]
+           (println result#)
+           (recur))))))
+
 (defn dispatch-form [form]
   (first form))
 
 (defmulti debug-form dispatch-form)
 
-
 (defmethod debug-form :default
   [form]
-  (println form)
   `(do
-     (repl)
+     (println (quote ~form))
+     ;; (flush)
+     ;; (repl3)
+     (repl*)
+     ;; (read)
      ~form))
 
 (defmethod debug-form 'let
   [[_ binds body]]
+
   (let [syms (take-nth 2 binds)
         vals (take-nth 2 (rest binds))
         vals-dbg (map debug-form vals)
-        binds* (vec (interleave syms vals-dbg))
-        ]
-    (list 'let binds* body)
-    ))
+        binds* (vec (interleave syms vals-dbg))]
+    (list 'let binds* body)))
 
-(defn bar [pairs]
-  (if (empty? pairs)
-    nil
-    (let [head (first pairs)
-          tail (rest pairs)]
-      (list 'let [head] (bar tail)))))
+(defn with-repl [form]
+  (println form)
+  (repl)
+  form)
+
+;; (defn bar [binds body]
+;;   (if (empty? binds)
+;;     body
+;;     (let [[sym form & tail] binds]
+;;       (list 'let [sym (list 'do repl-code form)] (bar tail body)))))
 
 (defmacro debug [& body]
   (cons
    'do
    (for [form body]
-     (debug-form form)
-     )
-
-        )
-  )
+     (debug-form form))))
